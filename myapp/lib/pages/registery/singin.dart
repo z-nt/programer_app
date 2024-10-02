005ab37c14d82a649c2136db16f77029fa9014e8
kian <kiannosratian76@gmail.com>
2024-10-03 02:45:13 +0330

    modified singin page

--- myapp/lib/pages/registery/singin.dart
+++ myapp/lib/pages/registery/singin.dart
@@ -13,24 +13,84 @@ class _SingInState extends State<SingIn> {
   Widget build(BuildContext context) {
       return Scaffold(
             appBar:AppBar(
-              title: Text('singIn'),
+              title: Text('MyApp'),
+              titleTextStyle: TextStyle(
+                color: Colors.black,
+                fontSize: 42,
+              ),
+              centerTitle: true,
             ),
-            body:Column(
-              children:<Widget>[
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Container(
-                    color: Colors.blue[2],
-                    width: 200,
-                    height: 299,
-                  ),
+            body:Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Center(
+                child: Column(
+                  children:<Widget>[
+                    
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: TextFormField(
+                          decoration: InputDecoration(
+                            border: OutlineInputBorder(),
+                            hintText: 'UserName',
+                          ),
+                      ),
+                    ),
+                
+                
+                    
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: TextFormField(
+                          decoration: InputDecoration(
+                            border: OutlineInputBorder(),
+                            hintText: 'PassWord',
+                          ),
+                      ),
+                    ),
+                
+                   
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Row(
+                        children:<Widget>[
+                          Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: ElevatedButton(
+                                onPressed: (){
+                                  context.goNamed("regis");
+                                },
+                                child: const Text('Login')
+                                ),
+                          ),
+                                    
+                                    
+                                    
+                                    
+                            Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: ElevatedButton(
+                                onPressed: (){
+                                 
+                                },
+                                child:Text('forgetPasword..!') 
+                                ),
+                            ),
+                                    
+                            Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: ElevatedButton(
+                                onPressed: (){
+                                 
+                                },
+                                child:Text('Register') 
+                                ),
+                            ),
+                        ],
+                      ),
+                    )
+                  ],
                 ),
-                ElevatedButton(
-                onPressed: (){
-                  context.goNamed("regis");
-                },
-                child: const Text('go to register'))
-              ],
+              ),
             ),
       );
   }

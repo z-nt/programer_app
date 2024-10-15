cb5ad0bb701610891bd5eaf1652162d6a1db6abc
kian <kiannosratian76@gmail.com>
2024-10-16 02:39:24 +0330

    modified regis

--- myapp/lib/pages/registery/Login/regis.dart
+++ myapp/lib/pages/registery/Login/regis.dart
@@ -1,4 +1,5 @@
 import  'package:flutter/material.dart';
+import 'package:flutter/semantics.dart';
 import 'package:myapp/pages/styles/style.dart';
 import 'package:myapp/pages/widget/widget.dart';
 
@@ -13,20 +14,74 @@ class Registeration extends StatelessWidget {
             appBar:AppBar(
               title: Text('Registeration'),
             ),
-            body: Center(
-              child: Container(
-                decoration: BoxDecoration(
-                  border: Border.all(color: Colors.red)
-                ),
-                width: 600,
-                height:100,
+
+            body:  Container(
+               decoration: BoxDecoration(
+                         border: Border.all(color:Colors.green)
+                       ),
+                height: 400,
+                  foregroundDecoration: BoxDecoration(),
+                  alignment: Alignment.centerLeft,
                 child: Padding(
                   padding: const EdgeInsets.all(8.0),
-                    child: FormInput(text: 'send code to your number', name:'phoneNumber' )
+                  child: Column(
+                    
+                    mainAxisSize: MainAxisSize.min,
+                    children:<Widget>[
+                      
+                        Semantics(
+                          sortKey: OrdinalSortKey(1.0),
+                          child:  FormInput(text: 'send a code to your number', name: 'phoneNumber'),
+                        ),
+                       
+                        Semantics(
+                          sortKey: OrdinalSortKey(2.0),
+                          child:  ButtonWidget( name: 'send'),
+                        )
+                       
+                  
+                        
+                                
+                               
+                  
+                  
+                    ],
+                  ),
                 ),
-              ),
+
+
+
             ),
+
+
+
+
+
+            // body: Container(
+            //   // child: Column(
+            //   //   children:<Widget>[
+                   
+            //   //       Container(
+            //   //         decoration: BoxDecoration(
+            //   //           border: Border.all(color:Colors.red)
+            //   //         ),
+            //   //         width: 600,
+            //   //         height: 200,
+            //   //         child: Padding(
+            //   //           padding: const EdgeInsets.all(8.0),
+            //   //           child: const ButtonWidget(name: 'send')
+            //   //         ),    
+            //   //     ),
+                 
+
+
+
+
+            //   //   ],
             
+              
+            //   ),
+            // ),
       );
   }
 
--- myapp/lib/pages/widget/widget.dart
+++ myapp/lib/pages/widget/widget.dart
@@ -2,36 +2,93 @@
 
 import 'package:flutter/material.dart';
 
-class FormInput extends StatelessWidget {
-  final String name;
-  final String text;
 
 
 
-const FormInput ({ super.key ,   required  this.text ,  required  this.name});
+// ///form input widget
+// class FormInput extends StatelessWidget {
+//   final String name;
+//   final String text;
+// const FormInput ({ super.key ,   required  this.text ,  required  this.name});
 
-  @override
-  Widget build(BuildContext context) {
-      return Center(
-        child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
-              TextFormField(
-                textAlign: TextAlign.center,
-                decoration:InputDecoration(
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(12),
-                    ),
-                  hintText: text,
-                  labelText: name
-                ) ,
-              )
-        ],),
+//   @override
+//   Widget build(BuildContext context) {
+//       return Center(
+//         child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+//               TextFormField(
+//                 textAlign: TextAlign.center,
+//                 decoration:InputDecoration(
+//                     border: OutlineInputBorder(
+//                       borderRadius: BorderRadius.circular(12),
+//                     ),
+//                   hintText: text,
+//                   labelText: name
+//                 ) ,
+//               )
+//         ],),
         
+//       );
+//   }
+// }
+
+// ///button widget
+class ButtonWidget extends StatelessWidget {
+
+    final String name;
+
+const ButtonWidget ({super.key , required this.name});
+
+@override
+  Widget build(BuildContext context) {
+
+      return Center(
+        child: Row(   children: [
+          OutlinedButton(        
+            style: ButtonStyle(
+             
+            ),
+              onPressed: (){},
+              child: Text(name),
+          ),
+        ], ),
       );
 
 
   }
-
-
-
-
 }
+
+///button widget
+class FormInput extends StatelessWidget {
+     final String name;
+  final String text;
+const FormInput ({ super.key ,   required  this.text ,  required  this.name});
+@override
+  Widget build(BuildContext context) {
+      return Center(
+        
+                child: Row(
+                
+                  children: [
+                    SizedBox(  
+                      width: 318,
+                      height: 42,
+                      child: TextFormField(
+                        textAlign: TextAlign.left,
+                        decoration: InputDecoration(
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(12)
+                          ),
+                            hintText: text,
+                            labelText: name
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+      );
+
+
+  }
+}
+
+
